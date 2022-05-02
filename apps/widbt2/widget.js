2a69bce7a8da3ec23dff29b549dff8e491a8642e
Rasmus Hollin <rasmus.hollin@jula.se>
2022-05-02 22:51:40 +0200

    Separate draw from change

--- apps/widbt2/widget.js
+++ apps/widbt2/widget.js
@@ -1,9 +1,11 @@
-WIDGETS["bluetooth2"]={area:"tr",width:15,draw:function() {
+WIDGETS.bluetooth2={area:"tr",width:15,draw:function() {
   g.reset();
   if (NRF.getSecurityStatus().connected) {
     g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
 	g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
   }
+},changed:function() {
+  WIDGETS.bluetooth2.draw();
 }};
-NRF.on('connect',WIDGETS["bluetooth2"].draw);
-NRF.on('disconnect',WIDGETS["bluetooth2"].draw);
+NRF.on('connect',WIDGETS.bluetooth2.changed);
+NRF.on('disconnect',WIDGETS.bluetooth2.changed);
